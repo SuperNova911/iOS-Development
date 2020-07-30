e818fe93471104db791497501571cfa3eab13119
Suwhan Kim <suwhan77@naver.com>
2020-07-30 21:39:25 +0900

    Lecture 3: Reactive UI + Protocols + Layout

---
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,58 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by 김수환 on 2020/07/28.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    viewModel.choose(card: card)
+                }
+            }
+        }
+        .foregroundColor(Color.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
+                }
+            }
+            .aspectRatio(2 / 3, contentMode: ContentMode.fit)
+            .font(Font.system(size: fontSize(for: geometry.size)))
+        }
+    }
+    
+    // MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    }
+}
