9404af6500e2e614228a7aba8b97185938e67b14
Suwhan Kim <suwhan77@naver.com>
2020-08-02 17:38:37 +0900

    Lecture 6: Animation

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -14,10 +14,12 @@ struct EmojiMemoryGameView: View {
         NavigationView {
             VStack {
                 Grid(viewModel.cards) { card in
-                    CardView(card: card).onTapGesture {
-                        viewModel.choose(card: card)
-                    }
-                    .padding(cardPadding)
+                        CardView(card: card).onTapGesture {
+                            withAnimation(.linear(duration: cardFlipDuration)) {
+                                viewModel.choose(card: card)
+                            }
+                        }
+                        .padding(cardPadding)
                 }
                 Text("Score: \(viewModel.score)")
                     .font(Font.largeTitle)
@@ -25,11 +27,16 @@ struct EmojiMemoryGameView: View {
             .padding()
             .foregroundColor(viewModel.themeColor)
             .navigationBarTitle(viewModel.themeName)
-            .navigationBarItems(trailing: Button(action: { viewModel.newGame() }, label: { Text("New Game") }))
+            .navigationBarItems(trailing: Button(action: { withAnimation(Animation.easeInOut) {
+                viewModel.newGame()
+            } }, label: { Text("New Game") }))
         }.navigationViewStyle(StackNavigationViewStyle())
     }
     
-    let cardPadding: CGFloat = 5;
+    // MARK: - Drawing Constants
+    
+    private let cardFlipDuration: Double = 0.75
+    private let cardPadding: CGFloat = 5;
 }
 
 struct CardView: View {
@@ -41,21 +48,51 @@ struct CardView: View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(110 - 90),
-                    clockwise: true)
-                    .padding(5).opacity(0.4)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(pieStartAngle), endAngle: Angle.degrees(-animatedBonusRemaining * 360 - 90),
+                            clockwise: true)
+                            .onAppear {
+                                startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle.degrees(pieStartAngle), endAngle: Angle.degrees(-card.bonusRemaining * 360 - 90),
+                            clockwise: true)
+                    }
+                }
+                .padding(piePadding).opacity(pieOpacity)
+                .transition(.identity)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? cardRotateDegrees : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: cardRotateDuration).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
     }
     
     // MARK: - Drawing Constants
+    
+    private let piePadding: CGFloat = 5
+    private let pieOpacity: Double = 0.4
+    private let pieStartAngle: Double = 0 - 90
+    private let pieEndAngle: Double = 110 - 90
+    private let cardRotateDegrees: Double = 360
+    private let cardRotateDuration: Double = 1
     
     private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.7
